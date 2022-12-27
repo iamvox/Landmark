fbefddf3a01f85362a898b27aea5aec32154e6c1
vox <drewdebelo@gmail.com>
2022-12-27 03:29:08 +0000

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Tom Finch on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
